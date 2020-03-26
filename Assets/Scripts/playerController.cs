c9e6343ce1386c53d28afb650f3f1e31a55f7d4e
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-03-26 20:03:46 +0530

    project created

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 11.0f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For left Boundary to halt the player
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // For right boundary to halt the player
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
