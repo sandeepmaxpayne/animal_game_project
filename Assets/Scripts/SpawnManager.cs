b9d002c6625a8e894f7cc126d376c59bf35143cf
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-03-27 18:14:17 +0530

    worked on spawning animals

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] animalPrefab;
+    private float spawnPosZ = 20;
+    private float spawnRangeX = 20;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+        }
+        
+    }
+}
